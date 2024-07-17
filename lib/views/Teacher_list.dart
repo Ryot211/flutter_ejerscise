b7d9200a5fae66c18c05b1cac2bc7bf68ce13ab1
Bryan Gallardo <gallarjoel@gmail.com>
2024-07-16 21:31:06 -0500

    Boton-Eliminar-profesor

--- lib/views/Teacher_list.dart
+++ lib/views/Teacher_list.dart
@@ -1,34 +1,92 @@
 import 'package:clase05_06_2024/entities/teacher_entiry.dart';
+import 'package:clase05_06_2024/settings/db_connection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-class Teacher_list extends StatelessWidget{
+class Teacher_list extends StatelessWidget {
   const Teacher_list({super.key});
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Listado de Profesores',style: TextStyle(color: Colors.white),),backgroundColor: Colors.black,
-    ),body: FutureBuilder<List<Teacher>>(
-      future: Teacher.select(),
-      builder: (context, snapshot){
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return Center (child: CircularProgressIndicator(),);
-        }else if (snapshot.hasError){
-          return Center(child: Text('Error: ${snapshot.error}'),);
-        }else{
-          List<Teacher> data = snapshot.data as List<Teacher>;
-          return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context,i){
-              return Card(
-                child: Row(children: [Text(data[i].name),Text(' '),Text(data[i].lastName)],),
-              );
-            });
-        }
-      }
-    ),
-    floatingActionButton: FloatingActionButton(onPressed: (){Navigator.pushNamed(context,'/Teacher/create');},child: Icon(Icons.add),)
-    );
-   
-    
+        appBar: AppBar(
+          title: Text(
+            'Listado de Profesores',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: FutureBuilder<List<Teacher>>(
+            future: Teacher.select(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
+                );
+              } else {
+                List<Teacher> data = snapshot.data as List<Teacher>;
+                return ListView.builder(
+                    itemCount: data.length,
+                    itemBuilder: (context, i) {
+                      return Card(
+                        child: Row(
+                          children: [
+                            Expanded(
+                                child: Row(
+                              children: [
+                                Text(data[i].name),
+                                Text(' '),
+                                Text(data[i].lastName)
+                              ],
+                            )),
+                            IconButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(context, '/Teacher/edit',
+                                      arguments: data[i]);
+                                },
+                                icon: Icon(Icons.edit)),
+                            IconButton(
+                                onPressed: () => showDialog<String>(
+                                      context: context,
+                                      builder: (BuildContext context) =>
+                                          AlertDialog(
+                                        title: const Text('Eliminar Profesor'),
+                                        content: Text(
+                                            'Estas Seguro de que quiere eliminar el profesor?'),
+                                        actions: [
+                                          TextButton(
+                                              onPressed: () async {
+                                                await DbConnection.delete(
+                                                    'Profesor',
+                                                    data[i].id as int);
+                                                Navigator.pushNamed(
+                                                    context, '/listTeacher');
+                                              },
+                                              child: Text('Aceptar')),
+                                          TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(
+                                                    context, 'Cancel');
+                                              },
+                                              child: Text('Cancelar'))
+                                        ],
+                                      ),
+                                    ),
+                                icon: Icon(Icons.delete))
+                          ],
+                        ),
+                      );
+                    });
+              }
+            }),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/Teacher/create');
+          },
+          child: Icon(Icons.add),
+        ));
   }
 }
---
+++ lib/views/teacher/editTe.dart
@@ -0,0 +1,111 @@
+import 'package:clase05_06_2024/entities/teacher_entiry.dart';
+import 'package:clase05_06_2024/settings/db_connection.dart';
+import 'package:flutter/material.dart';
+class TeacherEdit extends StatefulWidget{
+  @override
+  State<TeacherEdit> createState() => _TeacherEditState();
+}
+class _TeacherEditState extends State<TeacherEdit>{
+  final TeacherForm = GlobalKey<FormState>();
+  final NombreController = TextEditingController();
+  final LastNameController = TextEditingController();
+  final DeparmentController = TextEditingController();
+  final EmailController = TextEditingController();
+  final TelefonoController = TextEditingController();
+  @override
+  Widget build(BuildContext context){
+    final args = ModalRoute
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Teacher'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          key: TeacherForm,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: NombreController,
+                decoration: InputDecoration(labelText: 'Nombre:'),
+                validator: (value){
+              if (value == null || value.isEmpty){
+                return ' este campo es obligatorio';
+              }
+              return null;
+            },
+              ),
+                SizedBox(
+            height: 10,),
+              TextFormField(
+                controller: LastNameController,
+                decoration:  InputDecoration(labelText: 'Apellido:'),
+                validator: (value){
+              if (value == null || value.isEmpty){
+                return ' este campo es obligatorio';
+              }
+              return null;
+            },
+              ),
+                SizedBox(
+            height: 10,),
+              TextFormField(
+                controller: DeparmentController,
+                decoration:  InputDecoration(labelText: 'Departamento:'),
+                validator: (value){
+              if (value == null || value.isEmpty){
+                return ' este campo es obligatorio';
+              }
+              return null;
+            },
+              ),
+                SizedBox(
+            height: 10,),
+              TextFormField(
+                controller: EmailController,
+                decoration: InputDecoration(labelText: 'Correo electronico:'),
+                validator: (value){
+              if (value == null || value.isEmpty){
+                return ' este campo es obligatorio';
+              }
+              return null;
+            },
+              ),
+                SizedBox(
+            height: 10,),
+              TextFormField(
+                controller: TelefonoController,
+                decoration: InputDecoration(labelText: 'Telefono:'),
+                validator: (value){
+              if (value == null || value.isEmpty){
+                return ' este campo es obligatorio';
+              }
+              return null;
+            },
+                
+              ),
+              SizedBox (height: 20,),
+              ElevatedButton(onPressed: (){
+                insert();
+                Navigator.of(context).pushNamed('/listTeacher'); 
+              }, child: Text('Insertar Profesor'))
+            ]
+          ),
+      ),
+    ));
+  }
+  insert() async {
+    print ('Llegue a la funcion');
+    if(TeacherForm.currentState!.validate()){
+      TeacherForm.currentState!.save();
+      var data = Teacher(
+        name: NombreController.text, 
+        lastName: LastNameController.text, 
+        deparment: DeparmentController.text, 
+        email: EmailController.text, 
+        telefono: TelefonoController.hashCode,
+        );
+        print(await DbConnection.insert('Profesor', data.toDictionary()));
+    }
+  }
+}
